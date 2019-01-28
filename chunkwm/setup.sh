345d339cfe722d40a0fe59137c0c34d3e8587008
Felix H W <felixhow@icloud.com>
2019-01-28 04:45:26 +0100

    Add chunkwmrc, skhdrc, and setup script.

---
+++ chunkwm/setup.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+red="$(tput setaf 1)"
+green="$(tput setaf 2)"
+cyan="$(tput setaf 6)"
+reset="$(tput sgr0)"
+
+log() {
+	echo -e "${green}INFO: $1${reset}"
+}
+
+error() {
+	echo -e "${red}ERROR: $1${reset}"
+}
+
+initialize() {
+	read -r -p "${cyan}Setup chunkwm and skhd config? ${reset}[y/N] " answer
+	if [ "$answer" != y ] && [ "$answer" != Y ]; then
+		error "Setup declined..."
+		error "Now exiting."
+		exit
+	else
+		setup
+	fi
+}
+
+setup() {
+	echo -e "Setting up chunkwm and skhd config..."
+
+	timestamp=$(date +"%Y%m%d-%H:%M:%S")
+
+	if [ -f "$HOME/.chunkwmrc" ]; then
+		log "Making backup of '$HOME/.chunkwmrc' to '$HOME/.chunkwmrc.backup.$timestamp'"
+		mv $HOME/.chunkwmrc $HOME/.chunkwmrc.backup.$timestamp
+	fi
+
+	if [ -f "$HOME/.skhdrc" ]; then
+		log "Making backup of '$HOME/.skhdrc' to '$HOME/.skhdrc.backup.$timestamp'"
+		mv $HOME/.skhdrc $HOME/.skhdrc.backup.$timestamp
+	fi
+
+	log "Symlinking .chunkwmrc..."
+	ln -sf "$(PWD)/.chunkwmrc" $HOME
+
+	log "Symlinking .skhdrc..."
+	ln -sf "$(PWD)/.skhdrc" $HOME
+
+	log "Setup complete."
+}
+
+initialize
