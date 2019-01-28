a8755fda7512efaf2eb9d30d147a2a359fc25b13
Felix H W <felixhow@icloud.com>
2019-01-28 02:48:02 +0100

    Fix conditional statement in install script.

--- bash_prompt/install.sh
+++ bash_prompt/install.sh
@@ -4,25 +4,23 @@ red="$(tput setaf 1)"
 green="$(tput setaf 2)"
 cyan="$(tput setaf 6)"
 reset="$(tput sgr0)"
+newline='
+'
 
 log() {
 	echo -e "${green}INFO: $1${reset}"
 }
 
 clone() {
-	echo -e "${cyan}If you wish to clone this repo into a specific location, please specify the path below."
-	echo -e "Else press 'Enter' to clone into: '$(PWD)/dotfiles'"
-
-	read -r -p "${cyan}PATH:${reset} " answer
-
-	if [ $answer ]; then
+	read -r -p "${cyan}If you wish to clone this repo into a specific location, please specify the path.${newline}Else press 'Enter' to clone into: '$(PWD)/dotfiles'${newline}PATH:${reset} " answer
+	if [ "$answer" != "" ]; then
 		log "Cloning fehawen/dotfiles to '${clonepath}/dotfiles'"
 		git clone https://github.com/fehawen/dotfiles.git ${clonepath}/dotfiles
-		next "${clonepath}/dotfiles/setup.sh"
+		next "${clonepath}/dotfiles/bash_prompt/setup.sh"
 	else
 		log "Cloning fehawen/dotfiles to '$(PWD)/dotfiles'"
 		git clone https://github.com/fehawen/dotfiles.git
-		next "$(PWD)/dotfiles/setup.sh"
+		next "$(PWD)/dotfiles/bash_prompt/setup.sh"
 	fi
 
 	next
