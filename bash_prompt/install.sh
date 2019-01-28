e7278228e3c463a3ea9b1084b0c2b261d022b42a
Felix H W <felixhow@icloud.com>
2019-01-28 03:07:43 +0100

    Fix stupid typo in install script.

--- bash_prompt/install.sh
+++ bash_prompt/install.sh
@@ -12,10 +12,10 @@ log() {
 }
 
 clone() {
-	read -r -p "${cyan}If you wish to clone this repo into a specific location, please specify the path.${newline}Else press 'Enter' to clone into: '$(PWD)/dotfiles'${newline}PATH:${reset} " answer
-	if [ "$answer" != "" ]; then
+	read -r -p "If you wish to clone this repo into a specific location, please specify the path.${newline}Else press 'Enter' to clone into: '$(PWD)/dotfiles'${newline}${cyan}PATH:${reset} " clonepath
+	if [ "$clonepath" != "" ]; then
 		log "Cloning fehawen/dotfiles to '${clonepath}/dotfiles'"
-		git clone https://github.com/fehawen/dotfiles.git ${clonepath}/dotfiles
+		git clone https://github.com/fehawen/dotfiles.git $clonepath
 		next "${clonepath}/dotfiles/bash_prompt/setup.sh"
 	else
 		log "Cloning fehawen/dotfiles to '$(PWD)/dotfiles'"
