e734dfeae6744a061668df2a30aa64d77266f35c
Felix H W <felixhow@icloud.com>
2019-01-28 02:25:33 +0100

    Add corrections to install script.

--- bash_prompt/install.sh
+++ bash_prompt/install.sh
@@ -1,25 +1,35 @@
 #!/bin/bash
 
-source $(PWD)/setup.sh
+red="$(tput setaf 1)"
+green="$(tput setaf 2)"
+cyan="$(tput setaf 6)"
+reset="$(tput sgr0)"
+
+log() {
+	echo -e "${green}INFO: $1${reset}"
+}
 
 clone() {
 	echo -e "${cyan}If you wish to clone this repo into a specific location, please specify the path below."
 	echo -e "Else press 'Enter' to clone into: '$(PWD)/dotfiles'"
 
-	read -r -p "${cyan}PATH:${reset} " clonepath
+	read -r -p "${cyan}PATH:${reset} " answer
 
-	if [[ $clonepath == "" ]]; then
+	if [[ $answer == "" ]]; then
 		log "Cloning fehawen/dotfiles to '$(PWD)/dotfiles'"
 		git clone https://github.com/fehawen/dotfiles.git
+		next "$(PWD)/dotfiles/setup.sh"
 	else
 		log "Cloning fehawen/dotfiles to '${clonepath}/dotfiles'"
 		git clone https://github.com/fehawen/dotfiles.git ${clonepath}/dotfiles
+		next "${clonepath}/dotfiles/setup.sh"
 	fi
+
+	next
 }
 
 next() {
-	command $(PWD)/setup.sh
+	command $1
 }
 
 clone
-next
