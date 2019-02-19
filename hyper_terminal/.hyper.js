199d380109409ddbf72741f921f1052406dc5292
fehawen <felixhow@icloud.com>
2019-02-19 15:47:14 +0100

    Set default shell to zsh.

--- hyper_terminal/.hyper.js
+++ hyper_terminal/.hyper.js
@@ -121,7 +121,7 @@ module.exports = {
     //
     // PowerShell on Windows
     // - Example: `C:\\WINDOWS\\System32\\WindowsPowerShell\\v1.0\\powershell.exe`
-    shell: '',
+    shell: '/bin/zsh',
 
     // for setting shell arguments (i.e. for using interactive shellArgs: `['-i']`)
     // by default `['--login']` will be used
