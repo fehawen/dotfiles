3bafb296915cb1b679043b1f3ec268e1f7643f67
Felix H W <felixhow@icloud.com>
2019-09-17 12:22:37 +0200

    Begin tweaking util scripts for lockscreen etc

--- utils/setoutput.sh
+++ utils/setoutput.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
 EXTERNAL_OUTPUT="DP1"
-INTERNAL_OUTPUT="eDP1"
+INTERNAL_OUTPUT="eDP1" # xrandr | grep \ connected | awk '{print $1}' | grep \e
 
-xrandr | grep -w $EXTERNAL_OUTPUT | grep " connected "
+xrandr | grep -w $EXTERNAL_OUTPUT | grep \ connected
 
 if [ $? -eq 0 ]; then
 	xrandr --output $INTERNAL_OUTPUT --off --output $EXTERNAL_OUTPUT --auto
