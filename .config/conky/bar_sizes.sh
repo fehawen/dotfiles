0fbd385881d324d6f48a79063294d98ea0f057ad
Felix H W <felixhow@icloud.com>
2019-10-06 05:06:44 +0200

    Fix conky/i3bar section widths and i3 gaps size

--- .config/conky/bar_sizes.sh
+++ .config/conky/bar_sizes.sh
@@ -2,12 +2,9 @@
 
 bar_sections() {
 	screen_width=$(echo $(xrandr | grep \* | awk '{print $1}' | cut -dx -f1))
-	padding=16
 	bars=4
 
-	divide_bars=$(echo $((${screen_width} / ${bars})))
-
-	bar_size=$(echo $((${divide_bars} - ${padding})))
+	bar_size=$(echo $((${screen_width} / ${bars})))
 
 	echo ${bar_size}
 }
