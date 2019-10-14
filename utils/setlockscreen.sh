3ae3d41a1baa6aa87cf21c8a6e041d923cebdc95
Felix H W <felixhow@icloud.com>
2019-10-15 00:26:49 +0200

    Update i3 lockscreen script

--- utils/setlockscreen.sh
+++ utils/setlockscreen.sh
@@ -4,15 +4,13 @@
 
 screen_resolution=$(xrandr | grep \* | awk '{print $1}')
 
-#if [[ "$screen_resolution" == "3200x1800" ]]; then
-#	i3-msg 'exec --no-startup-id i3lock -i ~/Dotfiles/.wallpapers/wallpaper.png' &> /dev/null
-#elif [[ "$screen_resolution" == "3440x1440" ]]; then
-#	i3-msg 'exec --no-startup-id i3lock -i ~/Dotfiles/.wallpapers/wallpaper-3440x1440.png' &> /dev/null
-#elif [[ "$screen_resolution" == "1920x1080" ]]; then
-#	i3-msg 'exec --no-startup-id i3lock -i ~/Dotfiles/.wallpapers/wallpaper-1920x1080.png' &> /dev/null
-#else
-#	i3-msg 'exec --no-startup-id i3lock -c "#182226"' &> /dev/null
-#fi
-
-i3-msg 'exec --no-startup-id i3lock -c "#182226"' &> /dev/null
+if [[ "$screen_resolution" == "3200x1800" ]]; then
+	i3-msg 'exec --no-startup-id i3lock -i ~/.wallpapers/wallpaper-3200x1800.png' &> /dev/null
+elif [[ "$screen_resolution" == "3440x1440" ]]; then
+	i3-msg 'exec --no-startup-id i3lock -i ~/.wallpapers/wallpaper-3440x1440.png' &> /dev/null
+elif [[ "$screen_resolution" == "1920x1080" ]]; then
+	i3-msg 'exec --no-startup-id i3lock -i ~/.wallpapers/wallpaper-1920x1080.png' &> /dev/null
+else
+	i3-msg 'exec --no-startup-id i3lock -c "#0D202F"' &> /dev/null
+fi
 
