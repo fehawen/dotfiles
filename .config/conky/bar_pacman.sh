d5c32534f21cf1c7c32fce8213983af7a89a7490
Felix H W <felixhow@icloud.com>
2019-10-18 14:02:47 +0200

    Update(conky): Add pacman update calculation script

---
+++ .config/conky/bar_pacman.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+time_since_update() {
+	local last_update=$(grep Running\ \'pacman\ -Syu /var/log/pacman.log | tail -1 | awk '{print $1, $2}' | sed 's/[][]//g')
+
+	local update_timestamp=$(date -d "$last_update" +%s)
+	local current_timestap=$(date +%s)
+
+	local difference=$(($current_timestap - $update_timestamp))
+
+	local output=""
+
+	local weeks=$(( $difference / 604800 ))
+	local days=$(( $difference / 86400 ))
+	local hours=$(( $difference / 3600 ))
+
+	(( $weeks > 0 )) && output+="${weeks}w "
+	(( $days > 0 )) && output+="${days}d "
+	output+="${hours}h"
+
+	echo "${output}"
+}
+
+time_since_update
--- .config/conky/last_update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-LASTPAC_SECS=$(date -d $(grep --color=no Running\ \'pacman\ -S\ -y\ -u /var/log/pacman.log | tail -1 | awk '{print $1}' | sed "s/\[//" | awk -F "-" '{print $2,"/",$3,"/",$1}' | sed s/\ //g) +%s)
-CURRENT_SECS=$(date +%s)
-DIFF_SECS=$((CURRENT_SECS - $LASTPAC_SECS))
-
-printf '%dh:%dm:%ds\n' $(($DIFF_SECS/3600)) $(($DIFF_SECS%3600/60)) $(($DIFF_SECS%60))
