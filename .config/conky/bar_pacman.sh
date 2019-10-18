e0e8a83beef6116d27e371d6bc4ade99df71703a
Felix H W <felixhow@icloud.com>
2019-10-18 18:23:55 +0200

    Update time difference formatting

--- .config/conky/bar_pacman.sh
+++ .config/conky/bar_pacman.sh
@@ -3,20 +3,16 @@
 time_since_update() {
 	local last_update=$(grep Running\ \'pacman\ -Syu /var/log/pacman.log | tail -1 | awk '{print $1, $2}' | sed 's/[][]//g')
 
-	local update_timestamp=$(date -d "$last_update" +%s)
+	local update_timestamp=$(date -d "${last_update}" +%s)
 	local current_timestap=$(date +%s)
 
-	local difference=$(($current_timestap - $update_timestamp))
+	local difference=$((${current_timestap} - ${update_timestamp}))
 
 	local output=""
 
-	local weeks=$(( $difference / 604800 ))
-	local days=$(( $difference / 60 / 60 / 24 ))
-	local hours=$(( $difference / 60 / 60 % 24 ))
+	local duration=$(( ${difference} / 86400 ))
 
-	(( $weeks > 0 )) && output+="${weeks}w "
-	(( $days > 0 )) && output+="${days}d "
-	output+="${hours}h"
+	(( ${duration} == 1 )) && output="${duration} day" || output="${duration} days"
 
 	echo "${output}"
 }
