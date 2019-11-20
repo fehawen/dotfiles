7dfae28ea8cbc83ff8b2b0fac577cf82f36433bf
Felix H W <felixhow@icloud.com>
2019-11-20 17:34:33 +0100

    Use dynamic primary/secondary display names

--- .config/i3/i3monitor.sh
+++ .config/i3/i3monitor.sh
@@ -1,31 +1,12 @@
 #!/bin/bash
 
-#internal_output="$(xrandr | grep "primary" | awk '{print $1}')"
-#external_output="$(xrandr | grep -w "connected" | awk '{if(NR>1)print $1}')"
-#
-#if [[ -z "$external_output" ]]; then
-#	xrandr --auto
-#else
-#	xrandr --output $internal_output --off --output $external_output --auto
-#fi
-#
-#i3-msg "restart"
+declare internal_output="$(xrandr | grep "primary" | awk '{print $1}')"
+declare external_output="$(xrandr | grep -w "connected" | awk '{if(NR>1)print $1}')"
 
-echo "This script can make any of the files in this directory private."
-echo "Enter the number of the file you want to protect:"
+if [[ -z "$external_output" ]]; then
+	xrandr --auto
+else
+	xrandr --output $internal_output --off --output $external_output --auto
+fi
 
-PS3="Your choice: "
-QUIT="QUIT THIS PROGRAM - I feel safe now."
-
-select FILENAME in *;
-do
-  case $FILENAME in
-        "$QUIT")
-          echo "Exiting."
-          break
-          ;;
-        *)
-          echo "You picked $FILENAME ($REPLY)"
-          ;;
-  esac
-done
+i3-msg "restart"
