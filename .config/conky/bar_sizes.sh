a40fe5c7da483866f4308a10b46615d1cea5908a
Felix H W <felixhow@icloud.com>
2019-11-20 11:32:03 +0100

    Update bar section width calc script

--- .config/conky/bar_sizes.sh
+++ .config/conky/bar_sizes.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-screen_width=$(echo $(xrandr | grep \* | awk '{print $1}' | cut -dx -f1))
+# This works, but does not take into consideration possible xrandr scaling,
+# it only uses the monitor's highest listed internal resolution
+# screen_width=$(echo $(xrandr | grep \* | awk '{print $1}' | cut -dx -f1))
+
+# This works better as it uses the monitor's actual current resolution
+# If the --listmonitors flag prints out plural, try --listactivemonitors flag instead
+screen_width=$(echo $(xrandr --listmonitors | grep \+ | awk '{print $3}' | cut -d/ -f1))
 bars=4
 
 bar_size=$(echo $((${screen_width} / ${bars})))
