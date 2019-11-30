af6a428e8f2cb67a527f2a93ab6b88ca7d507421
Felix H W <felixhow@icloud.com>
2019-11-30 04:15:48 +0100

    No need to send through i3-msg

--- .config/i3/i3screenlock.sh
+++ .config/i3/i3screenlock.sh
@@ -5,12 +5,12 @@
 declare "screen_resolution=$(xrandr | grep \* | awk '{print $1}')"
 
 if [[ "$screen_resolution" == "3200x1800" ]]; then
-	i3-msg 'exec --no-startup-id i3lock -i ~/.wallpapers/wallpaper-3200x1800.png' &> /dev/null
+	i3lock -i ~/.wallpapers/wallpaper-3200x1800.png
 elif [[ "$screen_resolution" == "3440x1440" ]]; then
-	i3-msg 'exec --no-startup-id i3lock -i ~/.wallpapers/wallpaper-3440x1440.png' &> /dev/null
+	exec --no-startup-id i3lock -i ~/.wallpapers/wallpaper-3440x1440.png
 elif [[ "$screen_resolution" == "1920x1080" ]]; then
-	i3-msg 'exec --no-startup-id i3lock -i ~/.wallpapers/wallpaper-1920x1080.png' &> /dev/null
+	exec --no-startup-id i3lock -i ~/.wallpapers/wallpaper-1920x1080.png
 else
-	i3-msg 'exec --no-startup-id i3lock -c "#0D202F"' &> /dev/null
+	exec --no-startup-id i3lock -c "#0D202F"
 fi
 
