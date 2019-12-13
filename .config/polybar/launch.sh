d8a0b8124411f250f6a5a0e138d54550017687e5
Felix H W <felixhow@icloud.com>
2019-12-13 02:51:27 +0100

    Add polybar config

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch bar
+polybar middle &
+polybar right &
