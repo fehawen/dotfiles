7dfb639a0a6446c38d2df4f60962143d3758a6b7
Felix H W <felixhow@icloud.com>
2019-12-13 11:17:33 +0100

    Skip multiple monitor setup for now

--- .config/i3/i3monitor.sh
+++ .config/i3/i3monitor.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-declare internal_output="$(xrandr | grep "primary" | awk '{print $1}')"
-declare external_output_1="$(xrandr | grep -w "connected" | awk '{if(NR>1)print $1}')"
+internal_output="$(xrandr | grep "primary" | awk '{print $1}')"
+external_output="$(xrandr | grep -w "connected" | awk '{if(NR>1)print $1}')"
 
-# declare external_output_1="$(xrandr | grep -w "connected" | awk '{if(NR>1)print $1}' | sed -n 1p)"
-# declare external_output_2="$(xrandr | grep -w "connected" | awk '{if(NR>1)print $1}' | sed -n 2p)"
+# external_output_1="$(xrandr | grep -w "connected" | awk '{if(NR>1)print $1}' | sed -n 1p)"
+# external_output_2="$(xrandr | grep -w "connected" | awk '{if(NR>1)print $1}' | sed -n 2p)"
 
-if [[ -z "$external_output_1" ]]; then
+if [[ -z "$external_output" ]]; then
 	xrandr --auto
-elif [[ "$external_output_1" == "$external_output_2" ]]; then
-	xrandr --output $internal_output --off --output $external_output --auto
 else
-	xrandr --output $internal_output --off --output $external_output_1 --auto --output $external_output_2 --auto --right-of $external_output_1
+	xrandr --output $internal_output --off --output $external_output --auto
 fi
 
 # if [[ -z "$external_output_1" ]]; then
