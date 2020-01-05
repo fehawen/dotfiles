26d371622c1580bbb3c87ab168be7f21977807a6
Felix H W <felixhow@icloud.com>
2020-01-05 06:02:15 +0100

    Change pacupdate output format

--- .config/polybar/scripts/pacupdate.sh
+++ .config/polybar/scripts/pacupdate.sh
@@ -1,16 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
 last_update=$(grep Running\ \'pacman\ -Syu /var/log/pacman.log | tail -1 | awk '{print $1, $2}' | sed 's/[][]//g' | cut -dT -f1)
 
 update_timestamp=$(date -d "${last_update}" +%s)
 current_timestap=$(date +%s)
 
-difference=$((${current_timestap} - ${update_timestamp}))
+difference=$((current_timestap - update_timestamp))
 
 output=""
 
-duration=$(( ${difference} / 86400 ))
+duration=$(( difference / 86400 ))
 
-(( ${duration} == 1 )) && output="${duration} day" || output="${duration} days"
+(( duration == 1 )) && output="${duration} day ago" || output="${duration} days ago"
 
 echo "${output}"
