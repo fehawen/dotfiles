9ab65e35793bb55f1c27d8c6e7021ed08a5d6b1a
Felix H W <felixhow@icloud.com>
2020-01-15 05:31:27 +0100

    Clean up

--- .config/i3/i3popup.sh
+++ .config/i3/i3popup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-declare popup_width=$(($(xrandr --listmonitors | grep \+ | awk '{print $3}' | cut -d/ -f1) / 100 * 80))
-declare popup_height=$(($(xrandr --listmonitors | grep \+ | awk '{print $3}' | cut -dx -f2 | cut -d/ -f1) / 100 * 70))
+popup_width=$(($(xrandr --listmonitors | grep \+ | awk '{print $3}' | cut -d/ -f1) / 100 * 80))
+popup_height=$(($(xrandr --listmonitors | grep \+ | awk '{print $3}' | cut -dx -f2 | cut -d/ -f1) / 100 * 70))
 
 
 if [[ ! $(i3-msg -t get_tree | grep -o "popup") ]]; then
@@ -11,7 +11,3 @@ if [[ ! $(i3-msg -t get_tree | grep -o "popup") ]]; then
 else
 	i3-msg "[instance=\"popup\"] resize set ${popup_width} ${popup_height}; [instance=\"popup\"] scratchpad show; [instance=\"popup\"] move position center"
 fi
-
-test() {
-	i3-msg "[instance=\"popup\"] resize set ${popup_width} ${popup_height}; [instance=\"popup\"] scratchpad show; [instance=\"popup\"] move position center"
-}
