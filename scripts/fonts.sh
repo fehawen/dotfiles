58c2d2f04e62d2faf55655f2b7a00fec5596bbfc
Felix H W <felixhow@icloud.com>
2020-01-16 15:57:27 +0100

    Add fonts and font setup

---
+++ scripts/fonts.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+copy_fonts() {
+	src="${1}"
+	dest="${2}"
+
+	if [[ ! -d "${dest}" ]]; then
+		echo -e "\nDirectory ${dest} does not exist."
+		echo -e "\nCreating it...\n"
+		sudo mkdir -pv "${dest}"
+	fi
+
+	pushd "$PWD/${src}" > /dev/null
+
+	for FONT_DIR in *; do
+		if [[ -d ${FONT_DIR} ]]; then
+			echo -e "\nCopying ${FONT_DIR} from ${src} to ${dest}...\n"
+			sudo cp -rv "${FONT_DIR}" "${dest}/"
+		fi
+	done
+
+	cd "$(dirs -l -0)" && dirs -c
+}
+
+setup_fonts() {
+	echo -e "\nSetting up fonts..."
+
+	base_dir="../fonts"
+	global_dest="/usr/share/fonts"
+	local_dest="${HOME}/.local/share/fonts"
+
+	copy_fonts ${base_dir} ${global_dest}
+	copy_fonts ${base_dir} ${local_dest}
+
+	echo -e "\nDONE.\n"
+}
+
+setup_fonts
