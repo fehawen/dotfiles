098bda238ce34797e0257cc44ba3ef693a58a107
Felix H W <felixhow@icloud.com>
2020-01-20 20:38:22 +0100

    Add conky changes for probook machine

--- .config/conky/conky_left.lua
+++ .config/conky/conky_left.lua
@@ -1,7 +1,7 @@
 conky.config = {
 	alignment = 'top_left',
 	background = true,
-	default_color = 'C1D5DA',
+	default_color = '3E5661',
 	color0 = '30383F',
 	color1 = '855959',
 	color2 = '4C626A',
@@ -13,29 +13,37 @@ conky.config = {
 	double_buffer = true,
 	font = 'Share Tech Mono:size=14:style=regular',
 	draw_shades = false,
-	draw_borders = false,
-	border_inner_margin = 0,
-	border_outer_margin = 0,
-	border_width = 2,
+	draw_borders = true,
+	border_inner_margin = 10,
+	border_outer_margin = 10,
+	border_width = 1,
+	pad_percents = 0,
 	gap_x = 25,
-	gap_y = 50,
-	minimum_width= 350,
-	maximum_width= 350,
+	gap_y = 25,
+	maximum_width = 200,
+	minimum_width = 200,
+	minimum_height = 1029,
 	no_buffers = true,
+	out_to_console = false,
+	out_to_ncurses = false,
+	out_to_stderr = false,
+	out_to_x = true,
 	own_window = true,
 	own_window_type = 'override',
 	own_window_transparent = true,
 	update_interval = 1,
 	use_xft = true,
 	draw_graph_borders = false,
+	stippled_borders = 0,
+	uppercase = false,
 }
 
 conky.text = [[
 
 # DATE
-${voffset -33}${color6}${alignc}${font Share Tech Mono:size=40}${time %a, %b %d}${font}
+${color6}${alignc}${font Share Tech Mono:size=30}${time %a, %b %d}${font}
 
-${color4}${hr}
+${color0}${hr}
 
 # SYSTEM
 # USERNAME / HOSTNAME
@@ -59,7 +67,7 @@ ${alignr}${color6}${execi 3600 ~/.local/bin/pacupdate}
 ${color4}PACKAGES\
 ${alignr}${color5}${execi 3600 sudo pacman -Q | wc -l}
 
-${color4}${hr}
+${color0}${hr}
 
 # CONTROLS
 ${color6}CONTROLS
@@ -68,7 +76,7 @@ ${alignr}${color6}${exec pamixer --get-volume-human}
 ${color4}BACKLIGHT\
 ${alignr}${color5}${exec xbacklight -get | cut -d. -f1}%
 
-${color4}${hr}
+${color0}${hr}
 
 # BATTERY / STATUS
 ${color6}BATTERY
@@ -77,7 +85,7 @@ ${alignr}${color5}${battery_status}
 ${color4}PERCENTAGE\
 ${alignr}${if_match ${battery_percent}>20}${color2}${battery_percent}${else}${color1}${battery_percent}${endif}%
 
-${color4}${hr}
+${color0}${hr}
 
 # STORAGE
 ${color6}STORAGE
@@ -86,38 +94,38 @@ ${alignr}${color6}${fs_used /} of ${fs_size /}
 ${color4}TYPE /\
 ${alignr}${color2}${fs_type /}
 
-${color4}I/O DISK READS
-${diskiograph_read 50,350 4C626A 855959 -t}
+${color5}I/O DISK READS
+${diskiograph_read 40,200}
 
-${color4}I/O DISK WRITES
-${diskiograph_write 50,350 4C626A 855959 -t}
+${color2}I/O DISK WRITES
+${diskiograph_write 40,200}
 
 # MEMORY
 ${color6}MEMORY USAGE
-${color4}RAM\
-${alignr}${color5}${mem} of ${memmax}
 ${color4}BUFFERS\
 ${alignr}${color4}${buffers}
 ${color4}CACHED\
 ${alignr}${color4}${cached}
 ${color4}SWAP\
 ${alignr}${color2}${swap} of ${swapmax}
+${color4}RAM\
+${alignr}${color5}${mem} of ${memmax}
 
 # RAM BAR
-${memgraph 50,350 3E5661 855959 -t}
+${color4}${memgraph 40,200}
 
 # TOP RAM
 ${color6}TOP RAM\
 ${alignr}${color0}PID | NAME | RAM
-${color4}${top_mem pid 1}${goto 70}${color6}${top_mem name 1}${alignr}${color2}${top_mem mem 1}%
-${color4}${top_mem pid 2}${goto 70}${color6}${top_mem name 2}${alignr}${color2}${top_mem mem 2}%
-${color4}${top_mem pid 3}${goto 70}${color6}${top_mem name 3}${alignr}${color2}${top_mem mem 3}%
-${color4}${top_mem pid 4}${goto 70}${color6}${top_mem name 4}${alignr}${color2}${top_mem mem 4}%
-${color4}${top_mem pid 5}${goto 70}${color6}${top_mem name 5}${alignr}${color2}${top_mem mem 5}%
-${color4}${top_mem pid 6}${goto 70}${color6}${top_mem name 6}${alignr}${color2}${top_mem mem 6}%
-${color4}${top_mem pid 7}${goto 70}${color6}${top_mem name 7}${alignr}${color2}${top_mem mem 7}%
-${color4}${top_mem pid 8}${goto 70}${color6}${top_mem name 8}${alignr}${color2}${top_mem mem 8}%
-${color4}${top_mem pid 9}${goto 70}${color6}${top_mem name 9}${alignr}${color2}${top_mem mem 9}%
-${color4}${top_mem pid 10}${goto 70}${color6}${top_mem name 10}${alignr}${color2}${top_mem mem 10}%
+${color4}${top_mem pid 1}${goto 60}${color6}${top_mem name 1}${alignr}${color2}${top_mem mem 1}%
+${color4}${top_mem pid 2}${goto 60}${color6}${top_mem name 2}${alignr}${color2}${top_mem mem 2}%
+${color4}${top_mem pid 3}${goto 60}${color6}${top_mem name 3}${alignr}${color2}${top_mem mem 3}%
+${color4}${top_mem pid 4}${goto 60}${color6}${top_mem name 4}${alignr}${color2}${top_mem mem 4}%
+${color4}${top_mem pid 5}${goto 60}${color6}${top_mem name 5}${alignr}${color2}${top_mem mem 5}%
+${color4}${top_mem pid 6}${goto 60}${color6}${top_mem name 6}${alignr}${color2}${top_mem mem 6}%
+${color4}${top_mem pid 7}${goto 60}${color6}${top_mem name 7}${alignr}${color2}${top_mem mem 7}%
+${color4}${top_mem pid 8}${goto 60}${color6}${top_mem name 8}${alignr}${color2}${top_mem mem 8}%
+${color4}${top_mem pid 9}${goto 60}${color6}${top_mem name 9}${alignr}${color2}${top_mem mem 9}%
+${color4}${top_mem pid 10}${goto 60}${color6}${top_mem name 10}${alignr}${color2}${top_mem mem 10}%
 
 ]];
--- .config/conky/conky_right.lua
+++ .config/conky/conky_right.lua
@@ -1,7 +1,7 @@
 conky.config = {
 	alignment = 'top_right',
 	background = true,
-	default_color = 'C1D5DA',
+	default_color = '3E5661',
 	color0 = '30383F',
 	color1 = '855959',
 	color2 = '4C626A',
@@ -13,29 +13,37 @@ conky.config = {
 	double_buffer = true,
 	font = 'Share Tech Mono:size=14:style=regular',
 	draw_shades = false,
-	draw_borders = false,
-	border_inner_margin = 0,
-	border_outer_margin = 0,
-	border_width = 2,
+	draw_borders = true,
+	border_inner_margin = 10,
+	border_outer_margin = 10,
+	border_width = 1,
+	pad_percents = 0,
 	gap_x = 25,
-	gap_y = 50,
-	minimum_width= 350,
-	maximum_width= 350,
+	gap_y = 25,
+	maximum_width = 200,
+	minimum_width = 200,
+	minimum_height = 1029,
 	no_buffers = true,
+	out_to_console = false,
+	out_to_ncurses = false,
+	out_to_stderr = false,
+	out_to_x = true,
 	own_window = true,
 	own_window_type = 'override',
 	own_window_transparent = true,
 	update_interval = 1,
 	use_xft = true,
 	draw_graph_borders = false,
+	stippled_borders = 0,
+	uppercase = false,
 }
 
 conky.text = [[
 
 # TIME
-${voffset -33}${color6}${alignc}${font Share Tech Mono:size=40}${time %H:%M:%S}${font}
+${color6}${alignc}${font Share Tech Mono:size=40}${time %H:%M:%S}${font}
 
-${color4}${hr}
+${color0}${hr}
 
 # NETWORK
 ${color6}NETWORK STATUS
@@ -54,7 +62,7 @@ ${alignr}${color6}${execi 60 curl ipinfo.io/ip}
 ${color4}ACCESS POINT\
 ${alignr}${color4}${wireless_ap wlp2s0}
 
-${color4}${hr}
+${color0}${hr}
 
 ${color6}NETWORK TRAFFIC
 ${color4}DOWN\
@@ -62,7 +70,7 @@ ${alignr}${color2}${downspeedf wlp2s0} KiB/s (${totaldown wlp2s0})
 ${color4}UP\
 ${alignr}${color2}${upspeedf wlp2s0} KiB/s (${totalup wlp2s0})
 
-${color4}${hr}
+${color0}${hr}
 
 # CPU USAGE
 ${color6}CPU USAGE
@@ -71,50 +79,44 @@ ${alignr}${color5}${cpu}%
 ${color4}PROCESSES\
 ${alignr}${color5}${processes}
 
-${color4}${hr}
-
-# CPU FREQUENCY
-${color6}CPU FREQUENCY
-${color4}CPU \#1\
-${alignr}${color2}${freq_g 1}GHz
-${color4}CPU \#2\
-${alignr}${color2}${freq_g 2}GHz
-${color4}CPU \#3\
-${alignr}${color2}${freq_g 3}GHz
-${color4}CPU \#4\
-${alignr}${color2}${freq_g 4}GHz
-
-${color4}${hr}
+${color0}${hr}
 
 # CPU GRAPHS
-${color6}CPU LOAD
+${color6}LOAD\
+${alignr}FREQUENCY
 
-${color2}CPU \#1\
-${goto 175}${color5}CPU \#2
-${cpugraph cpu1 50,165 4C626A 855959 -t}\
-${goto 175}${cpugraph cpu2 50,175 5E7A85 855959 -t}
+${color6}CPU \#1\
+${alignr}${freq_g 1}GHz
+${cpugraph cpu1 40,200}
 
-${color6}CPU \#3\
-${goto 175}${color4}CPU \#4
-${cpugraph cpu3 50,165 72949F 855959 -t}\
-${goto 175}${cpugraph cpu4 50,175 3E5661 855959 -t}
+${color5}CPU \#2\
+${alignr}${freq_g 2}GHz
+${cpugraph cpu2 40,200}
+
+${color2}CPU \#3\
+${alignr}${freq_g 3}GHz
+${cpugraph cpu3 40,200}
+
+${color4}CPU \#4\
+${alignr}${freq_g 4}GHz
+${cpugraph cpu4 40,200}
 
 # LOAD GRAPH
 ${color6}SYSTEM LOAD AVERAGE
-${loadgraph 50,350 4C626A 855959 -t}
+${loadgraph 60,200}
 
 # TOP CPU
 ${color6}TOP CPU\
 ${alignr}${color0}PID | NAME | CPU
-${color4}${top pid 1}${goto 70}${color6}${top name 1}${alignr}${color2}${top cpu 1}%
-${color4}${top pid 2}${goto 70}${color6}${top name 2}${alignr}${color2}${top cpu 2}%
-${color4}${top pid 3}${goto 70}${color6}${top name 3}${alignr}${color2}${top cpu 3}%
-${color4}${top pid 4}${goto 70}${color6}${top name 4}${alignr}${color2}${top cpu 4}%
-${color4}${top pid 5}${goto 70}${color6}${top name 5}${alignr}${color2}${top cpu 5}%
-${color4}${top pid 6}${goto 70}${color6}${top name 6}${alignr}${color2}${top cpu 6}%
-${color4}${top pid 7}${goto 70}${color6}${top name 7}${alignr}${color2}${top cpu 7}%
-${color4}${top pid 8}${goto 70}${color6}${top name 8}${alignr}${color2}${top cpu 8}%
-${color4}${top pid 9}${goto 70}${color6}${top name 9}${alignr}${color2}${top cpu 9}%
-${color4}${top pid 10}${goto 70}${color6}${top name 10}${alignr}${color2}${top cpu 10}%
+${color4}${top pid 1}${goto 60}${color6}${top name 1}${alignr}${color2}${top cpu 1}%
+${color4}${top pid 2}${goto 60}${color6}${top name 2}${alignr}${color2}${top cpu 2}%
+${color4}${top pid 3}${goto 60}${color6}${top name 3}${alignr}${color2}${top cpu 3}%
+${color4}${top pid 4}${goto 60}${color6}${top name 4}${alignr}${color2}${top cpu 4}%
+${color4}${top pid 5}${goto 60}${color6}${top name 5}${alignr}${color2}${top cpu 5}%
+${color4}${top pid 6}${goto 60}${color6}${top name 6}${alignr}${color2}${top cpu 6}%
+${color4}${top pid 7}${goto 60}${color6}${top name 7}${alignr}${color2}${top cpu 7}%
+${color4}${top pid 8}${goto 60}${color6}${top name 8}${alignr}${color2}${top cpu 8}%
+${color4}${top pid 9}${goto 60}${color6}${top name 9}${alignr}${color2}${top cpu 9}%
+${color4}${top pid 10}${goto 60}${color6}${top name 10}${alignr}${color2}${top cpu 10}%
 
 ]];
