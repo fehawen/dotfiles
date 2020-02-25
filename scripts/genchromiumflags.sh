226b82577809ab027e7fb398b46c188d5420b483
Felix H W <felixhow@icloud.com>
2020-02-25 16:38:06 +0100

    Change qhd display scaling

--- scripts/genchromiumflags.sh
+++ scripts/genchromiumflags.sh
@@ -3,11 +3,11 @@
 PROGNAME=$(basename "$0")
 
 exit_on_fail() {
-	"$@" 2> /dev/null
-	local code=$?
-	if [[ ${code} -ne 0 ]]; then
-		echo -e "\nERROR: Command [$*] failed with error code ${code}\nFILE: ${PROGNAME}\nLINE: $LINENO\n" 1>&2
-	fi
+  "$@" 2> /dev/null
+  local code=$?
+  if [[ ${code} -ne 0 ]]; then
+    echo -e "\nERROR: Command [$*] failed with error code ${code}\nFILE: ${PROGNAME}\nLINE: $LINENO\n" 1>&2
+  fi
 }
 
 # Generate chromium-flags for scaling
@@ -16,8 +16,8 @@ DEST_DIR="${HOME}/.config"
 DEST_FILE="chromium-flags.conf"
 
 if [ ! -d "${DEST_DIR}" ]; then
-	echo -e "\nERROR: No ${DEST_DIR} directory found.\nExiting...\n"
-	exit 1
+  echo -e "\nERROR: No ${DEST_DIR} directory found.\nExiting...\n"
+  exit 1
 fi
 
 OUTPUT=""
@@ -25,27 +25,27 @@ OUTPUT=""
 PS3="Please select your computer model: "
 
 declare -a options=(
-	"Dell XPS 13 9343"
-	"HP ProBook 650 G3"
-	"Quit"
+  "Dell XPS 13 9343"
+  "HP ProBook 650 G3"
+  "Quit"
 )
 
 select OPT in "${options[@]}"
 do
-	case $OPT in
-		"Dell XPS 13 9343")
-			OUTPUT="--force-device-scale-factor=1.20"
-			break
-			;;
-		"HP ProBook 650 G3")
-			OUTPUT="--force-device-scale-factor=0.70"
-			break
-			;;
-		"Quit")
-			break
-			;;
-		*) echo -e "\nInvalid option: $REPLY\n";;
-	esac
+  case $OPT in
+    "Dell XPS 13 9343")
+      OUTPUT="--force-device-scale-factor=1.20"
+      break
+      ;;
+    "HP ProBook 650 G3")
+      OUTPUT="--force-device-scale-factor=0.80"
+      break
+      ;;
+    "Quit")
+      break
+      ;;
+    *) echo -e "\nInvalid option: $REPLY\n";;
+  esac
 done
 
 printf '%s' "$OUTPUT" > "${DEST_DIR}/${DEST_FILE}"
