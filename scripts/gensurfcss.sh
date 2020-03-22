d2352205da124e4275394d18a2f027722f5845a5
Felix H W <felixhow@icloud.com>
2020-03-22 17:59:59 +0100

    Use wal colors

--- scripts/gensurfcss.sh
+++ scripts/gensurfcss.sh
@@ -5,43 +5,42 @@
 PROGNAME=$(basename "$0")
 
 exit_on_fail() {
-	"$@" 2> /dev/null
-	local code=$?
-	if [[ ${code} -ne 0 ]]; then
-		echo -e "\nERROR: Command [$*] failed with error code ${code}\nFILE: ${PROGNAME}\nLINE: $LINENO\n" 1>&2
-	fi
+  "$@" 2> /dev/null
+  local code=$?
+  if [[ ${code} -ne 0 ]]; then
+    echo -e "\nERROR: Command [$*] failed with error code ${code}\nFILE: ${PROGNAME}\nLINE: $LINENO\n" 1>&2
+  fi
 }
+
+# shellcheck disable=SC1090
+source "$HOME/.cache/wal/colors.sh"
 
 DEST_DIR="${HOME}/.surf/styles"
 DEST_FILE="default.css"
 
 if [ ! -d "${DEST_DIR}" ]; then
-	exit_on_fail mkdir -pv "${DEST_DIR}"
+  exit_on_fail mkdir -pv "${DEST_DIR}"
 fi
 
-get_xres() {
-	exit_on_fail xrdb -query | grep "$1" | awk '{print $2}'
-}
-
 get_xfont() {
-	exit_on_fail xrdb -query | grep "font" | cut -d: -f3
+  exit_on_fail xrdb -query | grep "font" | cut -d: -f3
 }
 
-# set completion-bg "$(get_xres background:)"
+# shellcheck disable=SC2154
 OUTPUT="$(cat << CONF
 * {
-	font-family: "$(get_xfont)" !important;
-	background-color: $(get_xres background:) !important;
-	color: $(get_xres foreground:) !important;
-	border: none !important;
-	border-top: none !important;
-	border-right: none !important;
-	border-bottom: none !important;
-	border-left: none !important;
+  font-family: "$(get_xfont)" !important;
+  background-color: $background !important;
+  color: $foreground !important;
+  border: none !important;
+  border-top: none !important;
+  border-right: none !important;
+  border-bottom: none !important;
+  border-left: none !important;
 }
 
 *::before {
-	background-image: none !important;
+  background-image: none !important;
 }
 
 pre,
@@ -50,26 +49,26 @@ body,
 td,
 tr,
 p {
-	font-family: "$(get_xfont)" !important;
-	background-color: $(get_xres background:) !important;
-	color: $(get_xres foreground:) !important;
+  font-family: "$(get_xfont)" !important;
+  background-color: $background !important;
+  color: $foreground !important;
 }
 
 div {
-	font-family: "$(get_xfont)" !important;
-	background-color: $(get_xres background:) !important;
-	color: $(get_xres foreground:) !important;
+  font-family: "$(get_xfont)" !important;
+  background-color: $background !important;
+  color: $foreground !important;
 }
 
 h1 {
-	font-family: "$(get_xfont)" !important;
-	background-color: $(get_xres background:) !important;
-	color: $(get_xres foreground:) !important;
+  font-family: "$(get_xfont)" !important;
+  background-color: $background !important;
+  color: $foreground !important;
 }
 
 a h1 {
-	font-family: "$(get_xfont)" !important;
-	color: $(get_xres blue:) !important;
+  font-family: "$(get_xfont)" !important;
+  color: $color4 !important;
 }
 
 h2,
@@ -77,9 +76,9 @@ h3,
 h4,
 h5,
 h6 {
-	font-family: "$(get_xfont)" !important;
-	background-color: $(get_xres background:) !important;
-	color: $(get_xres foreground:) !important;
+  font-family: "$(get_xfont)" !important;
+  background-color: $background !important;
+  color: $foreground !important;
 }
 
 a h2,
@@ -87,36 +86,36 @@ a h3,
 a h4,
 a h5,
 a h6 {
-	font-family: "$(get_xfont)" !important;
-	color: $(get_xres blue:) !important;
+  font-family: "$(get_xfont)" !important;
+  color: $color4 !important;
 }
 
 a p {
-	font-family: "$(get_xfont)" !important;
-	color: $(get_xres green:) !important;
+  font-family: "$(get_xfont)" !important;
+  color: $color2 !important;
 }
 
 a {
-	font-family: "$(get_xfont)" !important;
-	color: $(get_xres magenta:) !important;
-	background-color: $(get_xres background:) !important;
-	border: none !important;
+  font-family: "$(get_xfont)" !important;
+  color: $color5 !important;
+  background-color: $background !important;
+  border: none !important;
 }
 
 button {
-	font-family: "$(get_xfont)" !important;
-	background-color: $(get_xres background:) !important;
-	color: $(get_xres magenta:) !important;
-	border: none !important;
-	border-radius: 0 !important;
+  font-family: "$(get_xfont)" !important;
+  background-color: $background !important;
+  color: $color5 !important;
+  border: none !important;
+  border-radius: 0 !important;
 }
 
 summary {
-	font-family: "$(get_xfont)" !important;
-	background-color: $(get_xres background:) !important;
-	color: $(get_xres foreground:) !important;
-	border: none !important;
-	border-radius: 0 !important;
+  font-family: "$(get_xfont)" !important;
+  background-color: $background !important;
+  color: $foreground !important;
+  border: none !important;
+  border-radius: 0 !important;
 }
 
 button svg,
@@ -134,9 +133,9 @@ a svg,
 a i,
 a span,
 a div {
-	font-family: "$(get_xfont)" !important;
-	background-color: $(get_xres background:) !important;
-	color: $(get_xres magenta:) !important;
+  font-family: "$(get_xfont)" !important;
+  background-color: $background !important;
+  color: $color5 !important;
 }
 
 summary svg,
@@ -151,62 +150,62 @@ summary h4,
 summary h5,
 summary h6,
 button svg {
-	background-color: $(get_xres background:) !important;
-	color: $(get_xres magenta:) !important;
+  background-color: $background !important;
+  color: $color5 !important;
 }
 
 a svg {
-	fill: $(get_xres magenta:) !important;
+  fill: $color5 !important;
 }
 
 summary svg {
-	fill: $(get_xres magenta:) !important;
+  fill: $color5 !important;
 }
 
 svg {
-	fill: $(get_xres magenta:) !important;
+  fill: $color5 !important;
 }
 
 hr {
-	display: none !important;
+  display: none !important;
 }
 
 input, form {
-	font-family: "$(get_xfont)" !important;
-	background-color: $(get_xres background:) !important;
-	color: $(get_xres magenta:) !important;
-	border-radius: 0 !important;
+  font-family: "$(get_xfont)" !important;
+  background-color: $background !important;
+  color: $color5 !important;
+  border-radius: 0 !important;
 }
 
 input {
-	font-family: "$(get_xfont)" !important;
-	border: 1px solid $(get_xres magenta:) !important;
-	padding-left: 16px !important;
-	padding-right: 16px !important;
-	height: 32px !important;
+  font-family: "$(get_xfont)" !important;
+  border: 1px solid $color5 !important;
+  padding-left: 16px !important;
+  padding-right: 16px !important;
+  height: 32px !important;
 }
 
 label svg {
-	display: none !important;
+  display: none !important;
 }
 
 form {
-	font-family: "$(get_xfont)" !important;
-	border: none !important;
+  font-family: "$(get_xfont)" !important;
+  border: none !important;
 }
 
 pre,
 code {
-	color: $(get_xres yellow:) !important;
-	background: $(get_xres background:) !important;
-	border: none !important;
-	border-radius: 4px !important;
+  color: $color3 !important;
+  background: $background !important;
+  border: none !important;
+  border-radius: 4px !important;
 }
 
 pre span,
 code span {
-	color: $(get_xres magenta:) !important;
-	background: $(get_xres background:) !important;
+  color: $color5 !important;
+  background: $background !important;
 }
 
 [class*="cookie"],
@@ -215,8 +214,8 @@ code span {
 [id*="cookie"],
 [id^="cookie-"],
 [id$="-cookie"] {
-	display: none !important;
-	visibility: hidden !important;
+  display: none !important;
+  visibility: hidden !important;
 }
 CONF
 )"
