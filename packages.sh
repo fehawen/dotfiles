7059f3afe9e971eafd56689677037898493fe804
Felix Hägglund Wennergren <felixhow@icloud.com>
2020-09-01 10:15:43 +0200

    packages.sh: No bash

--- packages.sh
+++ packages.sh
@@ -1,64 +1,50 @@
-#!/usr/bin/env bash
+#!/bin/sh
+
+package_list() {
+    printf '%s ' \
+        "xorg-server" \
+        "xorg-xinit" \
+        "xorg-xrandr" \
+        "xorg-xsetroot" \
+        "xorg-xbacklight" \
+        "xf86-video-intel" \
+        "xf86-input-libinput" \
+        "gnu-free-fonts" \
+        "mesa" \
+        "man-db" \
+        "man-pages" \
+        "texinfo" \
+        "imagemagick" \
+        "alsa-utils" \
+        "alsa-lib" \
+        "pulseaudio" \
+        "pamixer" \
+        "dmenu" \
+        "w3m" \
+        "firefox" \
+        "chromium" \
+        "zip" \
+        "unzip" \
+        "p7zip" \
+        "npm" \
+        "docker" \
+        "docker-compose" \
+        "openssh" \
+        "zathura" \
+        "poppler" \
+        "zathura-pdf-poppler" \
+        "python-pip" \
+        "python-pillow" \
+        "jq" \
+        "noto-fonts-emoji" \
+        "gnu-netcat" \
+        "tree"
+}
 
 install_packages() {
     printf 'Installing packages for Arch Linux...\n'
 
-    packages=(
-        "xorg-server"
-        "xorg-xinit"
-        "xorg-xrandr"
-        "xorg-xsetroot"
-        "xorg-xbacklight"
-        "xf86-video-intel"
-        "xf86-input-libinput"
-        "gnu-free-fonts"
-        "mesa"
-        "man-db"
-        "man-pages"
-        "texinfo"
-        "imagemagick"
-        "alsa-utils"
-        "alsa-lib"
-        "pulseaudio"
-        "pamixer"
-        "dmenu"
-        "i3-gaps"
-        "i3lock"
-        "conky"
-        "dunst"
-        "kitty"
-        "w3m"
-        "picom"
-        "hsetroot"
-        "firefox"
-        "chromium"
-        "gimp"
-        "inkscape"
-        "gtop"
-        "ranger"
-        "neofetch"
-        "scrot"
-        "newsboat"
-        "zip"
-        "unzip"
-        "p7zip"
-        "npm"
-        "docker"
-        "docker-compose"
-        "openssh"
-        "zathura"
-        "poppler"
-        "zathura-pdf-poppler"
-        "python-pip"
-        "python-pillow"
-        "jshon"
-        "jq"
-        "noto-fonts-emoji"
-        "gnu-netcat"
-        "tree"
-    )
-
-    sudo pacman -Syu "${packages[@]}"
+    sudo pacman -Syu "$(package_list)"
 
     printf "Done.\n"
 }
